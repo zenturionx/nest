bbe5ce6a78768fb4aad58845889a5d00adb215e0
sebas <sebajjaque@falabella.cl>
2023-06-29 00:01:35 -0400

    Course 04: brand CRUD.

---
+++ 02-car-dealership/src/seed/seed.service.ts
@@ -0,0 +1,19 @@
+import {Injectable} from '@nestjs/common';
+import {CARS_SEED} from "./data/cars.seed";
+import {BRANDS_SEED} from "./data/brands.seed";
+import {CarsService} from "../cars/cars.service";
+import {BrandsService} from "../brands/brands.service";
+
+@Injectable()
+export class SeedService {
+    constructor(
+        private readonly carsService: CarsService,
+        private readonly brandsService: BrandsService
+    ) {
+    }
+    populateDB() {
+        this.carsService.fillCarsWithSeedData(CARS_SEED);
+        this.brandsService.fillBrandsWithSeedData(BRANDS_SEED);
+        return 'Seed executed';
+    }
+}
