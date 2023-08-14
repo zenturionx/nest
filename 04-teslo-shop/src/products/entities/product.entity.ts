86b22c666b4f90fee79c98997ace47341b6ef3d3
sebas <sebajjaque@falabella.cl>
2023-08-14 15:43:04 -0400

    Course 06: Teslo-shop Entity sin relaciones.

--- 04-teslo-shop/src/products/entities/product.entity.ts
+++ 04-teslo-shop/src/products/entities/product.entity.ts
@@ -10,4 +10,38 @@ export class Product {
         unique: true
     })
     title: string;
+
+    @Column('numeric', {
+        default: 0
+    })
+    price: number;
+
+    @Column( {
+        type: 'text',
+        nullable: true
+    })
+    description: string;
+
+    @Column( {
+        type: 'text',
+        unique: true
+    })
+    slug: string;
+
+    @Column( {
+        type: 'int',
+        default: 0
+    })
+    stock: number;
+
+    @Column( {
+        type: 'text',
+        array: true,
+    })
+    sizes: string[];
+
+    @Column( {
+        type: 'text',
+    })
+    gender: string;
 }
