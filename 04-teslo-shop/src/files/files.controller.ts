98823c3f63093e895d437860f010a10db2725909
sebas <sebajjaque@falabella.cl>
2023-08-17 21:02:54 -0400

    Course 06: Teslo-shop Servir archivos de manera controlada.

--- 04-teslo-shop/src/files/files.controller.ts
+++ 04-teslo-shop/src/files/files.controller.ts
@@ -1,12 +1,24 @@
-import {BadRequestException, Controller, Post, UploadedFile, UseInterceptors} from '@nestjs/common';
+import {BadRequestException, Controller, Get, Param, Post, Res, UploadedFile, UseInterceptors} from '@nestjs/common';
 import { FilesService } from './files.service';
 import {FileInterceptor} from "@nestjs/platform-express";
 import {fileFilter, fileNamer} from "./helpers";
 import {diskStorage} from "multer";
+import { Response } from 'express';
 
 @Controller('files')
 export class FilesController {
   constructor(private readonly filesService: FilesService) {}
+
+  @Get('product/:imageName')
+  findProductImage(
+      @Res() res: Response,
+      @Param('imageName') imageName: string
+  ) {
+
+    const path = this.filesService.getStaticProductImage(imageName);
+
+    res.sendFile(path);
+  }
 
   @Post('product')
   @UseInterceptors(FileInterceptor('file', {
@@ -24,8 +36,10 @@ export class FilesController {
       throw new BadRequestException('Make sure that the file is an image');
     }
 
+    const secureUrl = `${ file.filename }`;
+
     return {
-      fileName: file.originalname
+      secureUrl: secureUrl
     };
   }
 
