466abe2b429e9cb13e4d57ade37f6aa043dfc7a7
sebas <sebajjaque@falabella.cl>
2023-08-31 15:39:49 -0400

    Course 06:7: Auth módulos asíncronos.

--- 04-teslo-shop/src/auth/auth.module.ts
+++ 04-teslo-shop/src/auth/auth.module.ts
@@ -3,12 +3,36 @@ import {AuthService} from './auth.service';
 import {AuthController} from './auth.controller';
 import {TypeOrmModule} from "@nestjs/typeorm";
 import {User} from "./entities/user.entity";
+import {PassportModule} from "@nestjs/passport";
+import {JwtModule} from "@nestjs/jwt";
+import * as process from "process";
+import {ConfigModule, ConfigService} from "@nestjs/config";
 
 @Module({
     controllers: [AuthController],
     providers: [AuthService],
     imports: [
-        TypeOrmModule.forFeature([User])
+        TypeOrmModule.forFeature([User]),
+        PassportModule.register({defaultStrategy: 'jwt'}),
+        JwtModule.registerAsync({
+            imports: [ConfigModule],
+            inject: [ConfigService],
+            useFactory: (configService: ConfigService) => {
+                //console.log(configService.get('JWT_SECRET'))
+                return {
+                    secret: configService.get('JWT_SECRET'),
+                    signOptions: {
+                        expiresIn: '2h'
+                    }
+                }
+            }
+        })
+        // JwtModule.register({
+        //     secret: process.env.JWT_SECRET,
+        //     signOptions: {
+        //         expiresIn: '2h'
+        //     }
+        // })
     ],
     exports: [
         TypeOrmModule
